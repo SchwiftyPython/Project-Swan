d83b8e7e06365a3d212da6b016cb16062abd42f3
brian sauro <bhannasauro@hotmail.com>
2020-10-15 20:42:53 -0400

    Added GoRogue Framework Added Cell, Floor, Wall, GameMap, and Generator Classes

---
+++ Assets/Scripts/GameMap/Wall.cs
@@ -0,0 +1,12 @@
+﻿using GoRogue;
+using UnityEngine;
+
+namespace Assets.Scripts.Map
+{
+    public class Wall : Cell
+    {
+        public Wall(GameObject texture, Coord position) : base(texture, position, false, false)
+        {
+        }
+    }
+}
---
+++ Assets/Scripts/Generator.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Assets.Scripts.Map;
+using GoRogue;
+using GoRogue.MapGeneration;
+using GoRogue.MapViews;
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class Generator : MonoBehaviour
+    {
+        private void Start()
+        {
+            var terrainMap = new ArrayMap<bool>(200, 100);
+            QuickGenerators.GenerateRectangleMap(terrainMap);
+
+            var map = new Map.GameMap(terrainMap.Width, terrainMap.Height);
+
+            foreach (var position in terrainMap.Positions())
+            {
+                if (IsFloor(terrainMap[position]))
+                {
+                    //todo get floor prefab
+                    map.SetTerrain(new Floor(, position));
+                }
+                else
+                {
+                    //todo get wall prefab
+                    map.SetTerrain(new Wall(, position));
+                }
+            }
+        }
+
+        private static bool IsFloor(bool cellType)
+        {
+            return cellType;
+        }
+    }
+}
